15db154862debc3bc4d4371ce793606c53e07e24
Henri Happonen <78052199+henrihapponen@users.noreply.github.com>
2021-10-18 22:59:39 +0100

    Create main-v2.py

---
+++ main-v2.py
@@ -0,0 +1,72 @@
+# This version uses Python lists instead of pandas dataframe to to do the comparisons
+# It should be a bit faster...
+
+# Imports
+import pandas as pd
+from whoswho import who
+from difflib import SequenceMatcher
+import time
+
+# File directories and file names
+dir1 = '/Users/henrihapponen/Desktop/'
+file_master = 'authors.csv'
+
+dir2 = '/Users/henrihapponen/Desktop/'
+file_new = 'coauthors.csv'
+
+dir_output = '/Users/henrihapponen/Desktop/'
+file_output = 'Output.csv'
+
+# Start timer
+start_time = time.time()
+
+# Load data sets
+df_master = pd.read_csv(dir1 + file_master)
+df_master['Name'] = df_master['author_name']
+df_master['Source'] = 'Master Dataset'
+del df_master['author_name']
+
+df_new = pd.read_csv(dir2 + file_new)
+df_new['Name'] = df_new['coauthor_name']
+df_new['Source'] = 'New Dataset'
+del df_new['coauthor_name']
+
+# Make a full list of names
+master_list = df_master['Name'].tolist()
+new_list = df_new['Name'].tolist()
+full_list = master_list + new_list
+
+print(f'Length of full list before dropping identical matches: {len(full_list)}')
+
+# First remove all duplicates that are identical matches
+full_list = list(dict.fromkeys(full_list))
+
+print(f'Length of full list before dropping fuzzy matches: {len(full_list)}')
+
+# Then remove duplicates based on a high similarity ratio
+for index1, name1 in enumerate(full_list):
+    for index2, name2 in enumerate(full_list):
+
+        if index2 > index1:
+            similarity_ratio = SequenceMatcher(None, name1, name2).ratio()
+            similarity_score = who.ratio(name1, name2)
+
+            if similarity_ratio > 0.75 and similarity_score > 75:
+                del full_list[index2]
+            else:
+                continue
+        else:
+            continue
+
+print(f'Length of full list after dropping fuzzy matches: {len(full_list)}')
+
+# Create a dataframe for the resulting list
+df_out = pd.DataFrame()
+df_out['author_name'] = full_list
+
+# Save output as CSV
+df_out.to_csv(dir_output + file_output, index=False)
+
+time_elapsed_minutes = int((time.time() - start_time) / 60)
+time_elapsed_seconds = round((time.time() - start_time) % 60, 2)
+print(f'Time elapsed: {str(time_elapsed_minutes)} minutes and {str(time_elapsed_seconds)} seconds')
